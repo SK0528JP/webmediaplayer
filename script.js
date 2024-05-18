bbd8986f7e6b29d6ebbef09bb54b066c2f877ead
SK0528JP <142498919+SK0528JP@users.noreply.github.com>
2024-05-18 23:22:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ document.addEventListener('DOMContentLoaded', function() {
     const coverImage = document.getElementById('coverImage');
     const fileList = document.getElementById('fileList');
     const searchInput = document.getElementById('searchInput');
+    const currentTrack = document.getElementById('currentTrack');
 
-    // 音声ファイルと表示名のマッピング
     const audioFiles = [
         { file: 'audio/madeinabyss-oldstories.mp3', displayName: 'Made in Abyss - Old Stories' },
         { file: 'audio/madeinabyss-hanezevecaradhina.mp3', displayName: 'Made in Abyss - Hanezeve Caradhina' },
         { file: 'audio/genshin-arlecchinobgm.mp3', displayName: 'Genshin Arlecchino Theme' },
         { file: 'audio/genshin-partingoflightandshadow.mp3', displayName: 'Genshin Parting of light and Shadow 光と影の決別' },
         { file: 'audio/genshin-raidenbgm.mp3', displayName: 'Genshin Raiden Theme' }
-        // 他の音声ファイルをここに追加
     ];
 
-    // 音声ファイルのリストを表示
     function displayFiles(files) {
         fileList.innerHTML = '';
         files.forEach(entry => {
@@ -29,18 +27,15 @@ document.addEventListener('DOMContentLoaded', function() {
                 coverImage.src = coverSrc;
                 coverImage.style.display = 'block';
                 audioPlayer.play();
+                currentTrack.textContent = '再生中の曲: ' + entry.displayName;
             });
             fileList.appendChild(fileItem);
         });
     }
 
-    // アルファベット順に並び替え
     const sortedAudioFiles = audioFiles.sort((a, b) => a.displayName.localeCompare(b.displayName));
-    
-    // 初期リストを表示
     displayFiles(sortedAudioFiles);
 
-    // 検索機能の実装
     searchInput.addEventListener('input', function() {
         const query = searchInput.value.toLowerCase();
         const filteredFiles = sortedAudioFiles.filter(entry => 
